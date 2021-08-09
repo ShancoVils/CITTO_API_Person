f3c2c96ba515e0c843b9e408cc660594a61b75b8
Shanen <pernens@mail.ru>
2021-08-09 10:35:17 +0300

    Ten

--- person_manage/models.py
+++ person_manage/models.py
@@ -15,32 +15,44 @@ OFFICIAL_LIST =(
 
 
 class GroupPerson(models.Model):
-    Name_Group = models.CharField(max_length=30, blank=True)
-    data_created = models.DateTimeField(default=timezone.now)
-    data_update = models.DateTimeField( auto_now=True)
+    Name_Group = models.CharField(_('Название отдела'),max_length=30, blank=True)
+    data_created = models.DateTimeField(_('Дата создания'),default=timezone.now)
+    data_update = models.DateTimeField(_('Дата обновления'), auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Отделы"
     
 
     def __str__(self):
         return self.Name_Group
 
+
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    fio = models.CharField(max_length=30)
-    email = models.EmailField(_('email address'), unique=True)
-    name = models.CharField(max_length=15, blank=True )
-    Official = models.CharField(max_length=1, choices=OFFICIAL_LIST)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    data_created = models.DateTimeField(default=timezone.now)
-    data_update = models.DateTimeField( auto_now=True)
+    first_name = models.CharField(_('Фамилия'),max_length=30,blank=True)
+    namej = models.CharField(_('Имя'),max_length=30,blank=True)
+    last_name = models.CharField(_('Отчество'),max_length=30,blank=True)
+    email = models.EmailField(_('Почта'), unique=True)
+    official = models.CharField(_('Должность'),max_length=1, choices=OFFICIAL_LIST)
+    is_staff = models.BooleanField(_('Админ'),default=False)
+    is_active = models.BooleanField(_('Активирован'),default=False)
+    data_created = models.DateTimeField(_('Дата создания'),default=timezone.now)
+    data_update = models.DateTimeField(_('Дата обновления'), auto_now=True)
     date_joined = models.DateTimeField(default=timezone.now)
-    token_data = models.CharField(max_length=255,blank=True)
-    activate_code = models.CharField(max_length=255,blank=True)
-    person_group = ForeignKey(GroupPerson, on_delete=CASCADE, default="1")
+    token_data = models.CharField(_('Токен'),max_length=255,blank=True)
+    activate_code = models.CharField(_('Код активации'),max_length=255,blank=True)
+    person_group = ForeignKey(GroupPerson, on_delete=CASCADE, verbose_name="Отдел")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-    objects = CustomUserManager()
 
-    def __str__(self):
-        return self.email
 
+    def fio(self):
+        fio_field = f"{self.first_name} {self.last_name}  {self.namej}"
+        return fio_field
+        
+    class Meta:
+        verbose_name_plural = "Пользователи"
+
+
+
+   
